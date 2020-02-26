8196261ab0cec63c62167084f917db7206168dc6
Your Name <you@example.com>
2020-02-26 11:52:15 -0800

    Skills: Functions

--- functions.js
+++ functions.js
@@ -3,14 +3,41 @@
 
 // 1. isHometown
 
-// Define your function here
-
+function isHometown(town){
+  return town === 'San Francisco';
+}
 
 // 2. getFullName
 
-// Define your function here
+function getFullName(first_name, last_name){
+  return `${first_name} ${last_name}`;
+}
 
 
 // 3. calculateTotal
 
-// Define your function here
+function calculateTotal(base_price, state, tax = 0.05){
+  let subtotal = base_price * (1 + tax);
+  let fee = 0;
+  if( state === 'CA'){
+    fee = 0.03* subtotal;
+  }
+  else if (state === 'PA'){
+    fee = 2;
+  }
+  else if(state === 'MA'){
+    if(base_price <= 100){
+      fee = 1;
+    }
+    else{
+      fee = 3;
+    }
+  }
+  return subtotal+fee;
+}
+
+
+console.log(isHometown('San Francisco'));
+
+console.log(getFullName('Liz', 'Bruder'));
+console.log(calculateTotal(300, 'MA'));
