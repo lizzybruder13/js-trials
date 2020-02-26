8e8e11be605f7cd507c5b817bbdd8f8fd01b9ad0
Your Name <you@example.com>
2020-02-26 12:01:13 -0800

    working on Objects

--- objects.js
+++ objects.js
@@ -3,7 +3,18 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  let word_counts = {};
+
+  for(const word of phrase.split(' ')){
+    if(word in word_counts){
+      word_counts[word] += 1;
+    }
+    else{
+      word_counts[word] = 1;
+    }
+  }
+
+  return word_counts;
 }
 
 
@@ -11,3 +22,8 @@ function countWords(phrase) {
 function getMelonsAtPrice(price) {
   // Replace this with your code
 }
+
+
+
+const test = 'hello bye hello bye morning';
+console.log(countWords(test));
